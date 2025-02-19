a679a999960630afff29b556da62a3e876b94f4c
wwvl <120743919+wwvl@users.noreply.github.com>
2025-02-19 17:54:32 +0800

    build(vite): 优化项目构建配置
    - 在 package.json 中添加 vue-tsc 构建步骤，提高类型检查和编译效率
    - 在 vite.config.ts 中引入 @nuxt/ui 插件，增强 UI 配置
    - 调整代码格式，提高可读性

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
+import ui from "@nuxt/ui/vite";
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    ui({
+      ui: {
+        colors: {
+          primary: "green",
+          neutral: "slate",
+        },
+      },
+    }),
+  ],
+});
