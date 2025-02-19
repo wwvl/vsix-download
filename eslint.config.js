99354120fda727360553e66a537fe61bab2fd56a
wwvl <120743919+wwvl@users.noreply.github.com>
2025-02-19 18:06:42 +0800

    build: 添加 ESLint 配置文件
    - 新增 eslint.config.js 文件，采用 Antfu ESLint 配置
    - 配置 CSS、HTML 和 Markdown 文件的格式化规则
    - 设置忽略文件和目录，包括 .prettierrc.toml 和 **/*.yml

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// eslint.config.js
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    formatters: {
+      /**
+       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+       * By default uses Prettier
+       */
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+      /**
+       * Format Markdown files
+       * Supports Prettier and dprint
+       * By default uses Prettier
+       */
+      markdown: 'prettier',
+    },
+  },
+  {
+    ignores: ['.prettierrc.toml', '**/*.yml'],
+  },
+)
