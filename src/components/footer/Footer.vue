bc4768445384391b0fbba7aecbb0e2dbd85034ab
wwvl <120743919+wwvl@users.noreply.github.com>
2025-02-24 13:29:15 +0800

    style: 格式化 Footer 组件代码
    - 使用 ESLint 规则格式化了 Footer 组件的 TypeScript 代码
    - 调整了缩进、空格和换行，提高了代码可读性

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-  import type { FooterSlots } from '@/types/footer'
-  import { Primitive } from 'reka-ui'
+import type { FooterSlots } from '@/types/footer'
+import { Primitive } from 'reka-ui'
 
-  const slots = defineSlots<FooterSlots>()
+const slots = defineSlots<FooterSlots>()
 
-  const ui = {
-    root: '',
-    top: 'py-8 lg:py-12',
-    bottom: 'py-8 lg:py-12',
-    container: 'py-8 lg:py-4 lg:flex lg:items-center lg:justify-between lg:gap-x-3',
-    left: 'flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1',
-    center: 'mt-3 lg:mt-0 lg:order-2 flex items-center justify-center',
-    right: 'lg:flex-1 flex items-center justify-center lg:justify-end gap-x-1.5 lg:order-3',
-  }
+const ui = {
+  root: '',
+  top: 'py-8 lg:py-12',
+  bottom: 'py-8 lg:py-12',
+  container: 'py-8 lg:py-4 lg:flex lg:items-center lg:justify-between lg:gap-x-3',
+  left: 'flex items-center justify-center lg:justify-start lg:flex-1 gap-x-1.5 mt-3 lg:mt-0 lg:order-1',
+  center: 'mt-3 lg:mt-0 lg:order-2 flex items-center justify-center',
+  right: 'lg:flex-1 flex items-center justify-center lg:justify-end gap-x-1.5 lg:order-3',
+}
 </script>
 
 <template>
